96cf89ef551481ec14e46cba341085c2232db858
Tadeas Maca <maca.tadeas@gmail.com>
2026-05-06 12:07:42 +0200

    PV otazka 1

---
+++ PV/1/example_CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿int sum(int x, int y) {
+    return x + y;
+}
+
+void sum_ref(ref int result, int x, int y) {
+    result = x + y;
+}
+
+int sum1 = sum(1, 1);
+
+int sum2 = 0;
+sum_ref(ref sum2, 1, 1);
+
+Console.WriteLine($"sum1 = {sum1}, sum2 = {sum2}");
